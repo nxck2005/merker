1a3536c3ff07b0c3cb23c12183123ef5d0adf35e
Nishchal Ravi <nxckmusicprod@gmail.com>
2026-04-26 03:19:01 +0530

    Add euler and verlet integrators

--- merker/src/integrators.h
+++ merker/src/integrators.h
@@ -1,12 +1,51 @@
 #pragma once
 #include "bodies.h"
+#include <glm/geometric.hpp>
+
+using vehicle = masses::Vehicle;
+using body = masses::Body;
+using vec3 = glm::dvec3;
 
 namespace integrators {
-
+    /**  
+        Simpler but accumulates energy loss of 
+         the order of delta-t^2.
+    **/
     struct Euler {
-        
-    }
-    struct Verlet {
+        static void doTick(const body& parent, 
+                          vehicle& satellite, 
+                          double deltat) 
+        {   
+            // how far up is the satellite? (from center)
+            double r = glm::length(satellite.posVector);
 
+            // an arrow from the satellite to the centre of the body
+            vec3 dir = glm::normalize(-satellite.posVector);
+
+            // how much acceleration was added?
+            vec3 accel = (dir) * (parent.GM / (r * r));
+
+
+            satellite.velVector += accel * deltat;
+            satellite.posVector += satellite.velVector * deltat;
+        }
+    };
+
+    /** 
+        Orbits stay closed with leapfrog, as 
+        energy is conserved way better.
+    **/
+    struct Verlet {
+        static void doTick(const body& parent,
+                           vehicle& satellite,
+                           double deltat)
+        {
+            // leapfrog
+            vec3 acc      = glm::normalize(-satellite.posVector) * 
+                            (parent.GM / glm::dot(satellite.posVector, satellite.posVector));
+            satellite.posVector  += satellite.velVector * deltat + 0.5 * acc * deltat * deltat;
+            vec3 accNew   = glm::normalize(-satellite.posVector) * (parent.GM / glm::dot(satellite.posVector, satellite.posVector));
+            satellite.velVector  += 0.5 * (acc + accNew) * deltat;
+        }
     };
 }
