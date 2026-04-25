206fb0c72f6a90df6122b64bdca61584c061945c
Nishchal Ravi <nxckmusicprod@gmail.com>
2026-04-26 02:44:17 +0530

    add glm to build, properly

---
+++ merker/src/main.cpp
@@ -0,0 +1,9 @@
+#include "bodies.h"
+#include <glm/fwd.hpp>
+#include <print>
+
+int main() {
+    std::println("hello, world");
+    glm::dvec3 x = glm::dvec3(1.0, 2.0, 3.0);
+    std::println("glm initialized");
+}
--- merker/xmake.lua
+++ merker/xmake.lua
@@ -1,17 +1,6 @@
-add_rules("mode.debug", "mode.release")
 add_requires("glm")
 target("merker")
     add_packages("glm")
     set_kind("binary")
-    set_languages("c++20")
+    set_languages("c++23")
     add_files("src/*.cpp")
-
-    -- Vulkan
-    add_includedirs("C:/VulkanSDK/1.4.341.1/include") -- please change these includes later
-    add_linkdirs("C:/VulkanSDK/1.4.341.1/Lib")
-    add_links("vulkan-1")
-
-    -- GLFW and GLM via vcpkg
-    add_includedirs("C:/vcpkg/installed/x64-windows/include")
-    add_linkdirs("C:/vcpkg/installed/x64-windows/lib")
-    add_links("glfw3dll")
